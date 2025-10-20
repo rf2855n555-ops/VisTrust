378c6b274dbbcc6dd104124c7f411d51b92b6163
rf2855n555-ops <rf2855n555@privaterelay.appleid.com>
2025-10-19 20:48:36 -0500

    Update next.config.js

--- nextjs_space/next.config.js
+++ nextjs_space/next.config.js
@@ -1,15 +1,20 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: false,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
